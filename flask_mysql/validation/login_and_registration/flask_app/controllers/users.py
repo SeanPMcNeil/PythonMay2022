bcbf1f4382942b2a9d435e1426e780928b99e934
Sean McNeil <smcneil136@gmail.com>
2022-05-15 12:08:43 -0400

    Python | Login and Registration Assignment

---
+++ flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
@@ -0,0 +1,58 @@
+from crypt import methods
+from flask import render_template, request, redirect, session, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/register/user', methods=['POST'])
+def create_user():
+    if not User.validate_user(request.form):
+        return redirect('/')
+    
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+
+    data = {
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "email" : request.form['email'],
+        "password" : pw_hash
+    }
+
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect('/dashboard')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = {"email" : request.form['email']}
+    user_in_db = User.get_by_email(data)
+
+    if not user_in_db:
+        flash(u"Invalid Email/Password", "login")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash(u"Invalid Email/Password", "login")
+        return redirect('/')
+
+    session['user_id'] = user_in_db.id
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    data = {
+        'id' : session['user_id']
+    }
+    user = User.get_one(data)
+    print(user)
+    return render_template("dashboard.html", user = user)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
---
+++ flask_mysql/validation/login_and_registration/flask_app/models/user.py
@@ -0,0 +1,74 @@
+from unittest import result
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+PASSWORD_REGEX = re.compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$')
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW());"
+        return connectToMySQL("login_regis_schema").query_db(query, data)
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        result = connectToMySQL('login_regis_schema').query_db(query, data)
+        user = result
+        return user
+
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL('login_regis_schema').query_db(query, data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+
+        # First Name Check
+        if not user['first_name'].isalpha() or len(user['first_name']) < 3:
+            flash(u"First Name must be only letters AND at least 3 characters", "registration")
+            is_valid = False
+        
+        # Last Name Check
+        if not user['last_name'].isalpha() or len(user['last_name']) < 3:
+            flash(u"Last Name must be only letters AND at least 3 characters", "registration")
+            is_valid = False
+        
+        # Email Check
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL('login_regis_schema').query_db(query, user)
+        print(results)
+        if len(results) >= 1:
+            flash(u'Email is already registered', "registration")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash(u"Invalid email address!", "registration")
+            is_valid = False
+
+        # Password Length Check
+        if not PASSWORD_REGEX.match(user['password']):
+            flash(u"Password must be at least 8 characters with at least 1 number and 1 uppercase letter", "registration")
+            is_valid = False
+
+        # Password Confirmation Check
+        if user['password'] != user['confirm_password']:
+            flash(u"Passwords must match", "registration")
+            is_valid = False
+
+        return is_valid
